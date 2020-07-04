ecb33dbe16d6d3f6a81a6734124903ea76efdaae
wilson-pc <wilverhidalgo@outlook.com>
2020-07-03 19:26:31 -0500

    Update index.js
    tget54

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var app=require("./app");
 
 var port = process.env.port || 3002;
 
-app.listen(port,()=>{
+app.listen(process.env.PORT, '0.0.0.0',()=>{
     console.log("corriendo en puerto 3000");
 })
